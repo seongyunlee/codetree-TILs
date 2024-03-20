42bf62990e57d9201336afd1e1a02c51db88a1bf
Seongyun Lee <79950005+seongyunlee@users.noreply.github.com>
2024-03-20 17:50:47 +0900

    Title: 코드트리 메신저, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240320/코드트리 메신저/codetree-messenger.py
+++ 240320/코드트리 메신저/codetree-messenger.py
@@ -26,7 +26,7 @@ def update(idx):
             S[idx][k-1] = v
     elif len(CC)==1:
         for k,v in CC[0].items():
-            if k-1<=0:continue
+            if k-1<0:continue
             S[idx][k-1] = v
     if idx==0:return
     if S[idx].get(authority[idx])==None:
