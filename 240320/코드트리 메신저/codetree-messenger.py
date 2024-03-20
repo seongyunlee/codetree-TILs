1651b1a22ebc9bd61ebb091a1b558543887d8af9
Seongyun Lee <79950005+seongyunlee@users.noreply.github.com>
2024-03-20 18:11:18 +0900

    Title: 코드트리 메신저, Time: 2081ms, Memory: 76MB, Status: Passed - CodeTree

--- 240320/코드트리 메신저/codetree-messenger.py
+++ 240320/코드트리 메신저/codetree-messenger.py
@@ -1,81 +1,126 @@
-import sys
-input = sys.stdin.readline
-N,Q = map(int,input().split())
-K = list(map(int,input().split()))
-parent = [None] + K[1:N+1]
-child = [[] for _ in range(N+1)]
-for i in range(1,len(parent)):
-    child[parent[i]].append(i)
-authority = [None] + K[N+1:]
-alarm = [True]*(N+1)
-S = [{} for _ in range(N+1)]
-def sumDict(a,b):
-    s = dict(a)
-    for k,v in b.items():
-        if k in s:
-            s[k]+=v
-        else:s[k]=v
-    return s
-def update(idx):
-    S[idx] = {}
-    CC = [S[i] for i in child[idx] if alarm[i]]
-    if len(CC)==2:   
-        sumChild = sumDict(*CC) 
-        for k,v in sumChild.items():
-            if k-1<0:continue
-            S[idx][k-1] = v
-    elif len(CC)==1:
-        for k,v in CC[0].items():
-            if k-1<0:continue
-            S[idx][k-1] = v
-    if idx==0:return
-    if S[idx].get(authority[idx])==None:
-        S[idx][authority[idx]]=1
-    else:
-        S[idx][authority[idx]]+=1
-    if idx!=0:update(parent[idx])
-def makeInfo(idx):
-    CC = [makeInfo(i) for i in child[idx] if alarm[i]]
-    if len(CC)==2:   
-        sumChild = sumDict(*CC) 
-        for k,v in sumChild.items():
-            if k-1<0:continue
-            S[idx][k-1] = v
-    elif len(CC)==1:
-        for k,v in CC[0].items():
-            if k-1<0:continue
-            S[idx][k-1] = v
-    if idx==0:return
-    if S[idx].get(authority[idx])==None:
-        S[idx][authority[idx]]=1
-    else:
-        S[idx][authority[idx]]+=1
-    return S[idx]
-def toggleAlarm(idx):
-    alarm[idx] = not alarm[idx]
-    update(idx)
-def changeAuth(idx,N):
-    authority[idx] = N
-    update(idx)
-def changeParent(a,b):
-    pb,pa = parent[b],parent[a]
-    parent[b], parent[a] = pa,pb
-    child[pb].remove(b)
-    child[pb].append(a)
-    child[pa].remove(a)
-    child[pa].append(b)
-    update(pa)
-    update(pb)
-def query(idx):
-    return sum(S[idx].values())-(1 if idx!=0 else 0)
-makeInfo(0)
-for _ in range(Q-1):
-    M,*args = map(int,input().split())
-    if M==200:
-        toggleAlarm(*args)
-    elif M==300:
-        changeAuth(*args)
-    elif M==400:
-        changeParent(*args)
-    else:
-        print(query(*args))
+MAX_N = 100001
+MAX_D = 22
+
+n, q = 0, 0
+a, p, val = [0] * MAX_N, [0] * MAX_N, [0] * MAX_N
+noti = [False] * MAX_N
+nx = [[0 for _ in range(MAX_D)] for _ in range(MAX_N)]
+
+# 초기 설정 값을 받아옵니다.
+def init(inputs):
+    global n, a, p, val, nx
+    # 부모 채팅과 채팅의 권한 정보를 입력받습니다.
+    for i in range(1, n + 1):
+        p[i] = inputs[i]
+    
+    for i in range(1, n + 1):
+        a[i] = inputs[i + n]
+        # 채팅의 권한이 20을 초과하는 경우 20으로 제한합니다.
+        if a[i] > 20:
+            a[i] = 20
+    
+    # nx 배열과 val 값을 초기화합니다.
+    for i in range(1, n + 1):
+        cur = i
+        x = a[i]
+        nx[cur][x] += 1
+        # 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while p[cur] and x:
+            cur = p[cur]
+            x -= 1
+            if x:
+                nx[cur][x] += 1
+            val[cur] += 1
+
+# 채팅의 알림 상태를 토글합니다.
+def toggle_noti(chat):
+    cur = p[chat]
+    num = 1
+    # 상위 채팅으로 이동하며 noti 값에 따라 nx와 val 값을 갱신합니다.
+    while cur:
+        for i in range(num, 22):
+            val[cur] += nx[chat][i] if noti[chat] else -nx[chat][i]
+            if i > num:
+                nx[cur][i - num] += nx[chat][i] if noti[chat] else -nx[chat][i]
+        if noti[cur]:
+            break
+        cur = p[cur]
+        num += 1
+    noti[chat] = not noti[chat]
+
+# 채팅의 권한의 크기를 변경합니다.
+def change_power(chat, power):
+    bef_power = a[chat]
+    power = min(power, 20)  # 권한의 크기를 20으로 제한합니다.
+    a[chat] = power
+
+    nx[chat][bef_power] -= 1
+    if not noti[chat]:
+        cur = p[chat]
+        num = 1
+        # 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while cur:
+            if bef_power >= num:
+                val[cur] -= 1
+            if bef_power > num:
+                nx[cur][bef_power - num] -= 1
+            if noti[cur]:
+                break
+            cur = p[cur]
+            num += 1
+
+    nx[chat][power] += 1
+    if not noti[chat]:
+        cur = p[chat]
+        num = 1
+        # 상위 채팅으로 이동하며 nx와 val 값을 갱신합니다.
+        while cur:
+            if power >= num:
+                val[cur] += 1
+            if power > num:
+                nx[cur][power - num] += 1
+            if noti[cur]:
+                break
+            cur = p[cur]
+            num += 1
+
+# 두 채팅의 부모를 교체합니다.
+def change_parent(chat1, chat2):
+    bef_noti1 = noti[chat1]
+    bef_noti2 = noti[chat2]
+
+    if not noti[chat1]:
+        toggle_noti(chat1)
+    if not noti[chat2]:
+        toggle_noti(chat2)
+
+    p[chat1], p[chat2] = p[chat2], p[chat1]
+
+    if not bef_noti1:
+        toggle_noti(chat1)
+    if not bef_noti2:
+        toggle_noti(chat2)
+
+# 해당 채팅의 val 값을 출력합니다.
+def print_count(chat):
+    print(val[chat])
+
+
+n, q = map(int, input().split())
+for _ in range(q):
+    inputs = list(map(int, input().split()))
+    query = inputs[0]
+    if query == 100:
+        init(inputs)
+    elif query == 200:
+        chat = inputs[1]
+        toggle_noti(chat)
+    elif query == 300:
+        chat, power = inputs[1], inputs[2]
+        change_power(chat, power)
+    elif query == 400:
+        chat1, chat2 = inputs[1], inputs[2]
+        change_parent(chat1, chat2)
+    elif query == 500:
+        chat = inputs[1]
+        print_count(chat)
